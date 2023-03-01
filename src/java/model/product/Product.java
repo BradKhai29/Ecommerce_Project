df12f160ce92b6350c42622ab68892b5acd25975
bradfish29 <duongkhai2904@gmail.com>
2023-03-01 20:53:55 +0700

    Folder Product

--- src/java/model/product/Product.java
+++ src/java/model/product/Product.java
@@ -9,5 +9,72 @@ package model.product;
  * @author This PC
  */
 public class Product {
+    private final static String contextPath = "product/";
     
+    int productID;
+    String productName;
+    String imgURL;
+    int price;
+    String details;
+    boolean available;
+
+    public Product() {
+    }
+
+    public Product(int productID, String productName, String imgURL, int price, String details, boolean available) {
+        this.productID = productID;
+        this.productName = productName;
+        this.imgURL = imgURL;
+        this.price = price;
+        this.details = details;
+        this.available = available;
+    }
+
+    public int getProductID() {
+        return productID;
+    }
+
+    public void setProductID(int productID) {
+        this.productID = productID;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getImgURL() {
+        return imgURL;
+    }
+
+    public void setImgURL(String imgURL) {
+        this.imgURL = imgURL;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
 }
---
+++ src/java/model/product/ProductDAO.java
@@ -0,0 +1,61 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model.product;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+/**
+ *
+ * @author This PC
+ */
+public class ProductDAO extends model.DAO.BaseDAO<Product>{
+    private static String SELECT_ALL = "SELECT productName, price, p.priceCode, imgURL, details, available "
+                                     + "FROM Product p INNER JOIN ProductPrice pp ON p.priceCode = pp.priceCode";
+    
+    private static String SELECT =  "SELECT productName, price, p.priceCode, imgURL, details, available \n" +
+                                    "FROM Product p INNER JOIN ProductPrice pp ON p.priceCode = pp.priceCode\n" +
+                                    "WHERE productID = ?";
+    
+    @Override
+    protected void openQuery(String SQLQuery) {
+        
+    }
+
+    @Override
+    protected void closeQuery() {
+    
+    }
+
+    @Override
+    public Optional<Product> get(int id) {
+        Optional<Product> product = Optional.empty();
+        return product;
+    }
+
+    @Override
+    public Map<String, Product> getAll() {
+        Map<String, Product> products = new HashMap<>();
+        return products;
+    }
+
+    @Override
+    public void insert(Product obj) {
+    }
+
+    @Override
+    public void update(int id, String updateField, String updateValue) {
+    }
+
+    @Override
+    public void update(int id, String... updateValue) {
+    }
+
+    @Override
+    public void delete(int id) {
+    }
+    
+}
